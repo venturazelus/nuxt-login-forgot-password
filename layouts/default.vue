df6799bf2d06624a1fed9d65f9e04869297fd02d
ventura zelus <you@example.com>
2021-09-08 16:14:14 +0700

    Nuxt Login Page & Fprgot Passwprd

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app light>
+   
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    
+  </v-app>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
